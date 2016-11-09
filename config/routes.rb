f965da3117074aab1be3c48be18d921d434f20c4
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2016-11-09 17:02:07 -0500

    Some minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  # Google auth
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    provider "MyString"
+    uid "MyString"
+    name "MyString"
+    oauth_token "MyString"
+    oauth_expires_at "2016-10-25 20:06:03"
+  end
+end
